3d9e08d8d4d6b36b02e27aeab83cee1c08151539
Shawn Dempsey <shawn@shawndempsey.ca>
2017-07-20 20:46:53 -0600

    updating event order to date asc

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -60,7 +60,7 @@ function query_featured_event(){
         'meta_key' => 'event_date',
         'meta_type' => 'DATETIME',
         'orderby' => 'meta_value',
-        'order' => 'DESC',
+        'order' => 'ASC',
     );
     $post_query = new WP_Query($args);
     return $post_query;
@@ -82,7 +82,7 @@ function query_promotable_events(){
         'meta_key' => 'event_date',
         'meta_type' => 'DATETIME',
         'orderby' => 'meta_value',
-        'order' => 'DESC',
+        'order' => 'ASC',
     );
     $post_query = new WP_Query($args);
     return $post_query;
@@ -105,7 +105,7 @@ function query_front_page_events_panel(){
         'meta_key' => 'event_date',
         'meta_type' => 'DATETIME',
         'orderby' => 'meta_value',
-        'order' => 'DESC',
+        'order' => 'ASC',
     );
     $post_query = new WP_Query($args);
     return $post_query;
