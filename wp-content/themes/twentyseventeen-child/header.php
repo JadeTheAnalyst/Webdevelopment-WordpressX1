7f574673d18d3a319d6d30cca7202ae940c0e908
Shawn Dempsey <shawn@7shifts.com>
2017-07-24 22:44:44 -0600

    adding same meta description to each page

--- wp-content/themes/twentyseventeen-child/header.php
+++ wp-content/themes/twentyseventeen-child/header.php
@@ -17,6 +17,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Providing consistent support and skill development for people interested in reaching their potential based on John C. Maxwell materials. Facilitated by one of his certified team members, Jade Jalali.">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
