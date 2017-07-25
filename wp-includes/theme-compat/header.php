261e1bef6a7d3cd1a672921dd89c353e5a71b1af
Shawn Dempsey <shawn@7shifts.com>
2017-07-25 10:49:39 -0600

    adding ga tracking code

--- wp-includes/theme-compat/header.php
+++ wp-includes/theme-compat/header.php
@@ -26,6 +26,17 @@ _deprecated_file(
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-37697144-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <?php if ( file_exists( get_stylesheet_directory() . '/images/kubrickbgwide.jpg' ) ) { ?>
 <style type="text/css" media="screen">
 
