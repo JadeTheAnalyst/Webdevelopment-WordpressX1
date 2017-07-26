1935cd80e9ad0a8621c63beec0450aa4c87d5a96
Shawn Dempsey <shawn@7shifts.com>
2017-07-26 12:48:25 -0600

    adding logo image and updating meta description

--- wp-content/themes/twentyseventeen-child/header.php
+++ wp-content/themes/twentyseventeen-child/header.php
@@ -17,7 +17,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Providing consistent support and skill development for people interested in reaching their potential based on John C. Maxwell materials. Facilitated by one of his certified team members, Jade Jalali.">
+<meta name="description" content="Providing consistent support and skill development for people interested in reaching their potential. Based on John C. Maxwell materials and facilitated by one of his certified team members, Jade Jalali.">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
