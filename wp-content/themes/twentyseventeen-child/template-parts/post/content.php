0b0bddc0798b6af6e76dfd2fe134e674004b6531
Shawn Dempsey <shawn@7shifts.com>
2017-08-06 11:19:22 -0600

    putting dynamic post content back into events

--- wp-content/themes/twentyseventeen-child/template-parts/post/content.php
+++ wp-content/themes/twentyseventeen-child/template-parts/post/content.php
@@ -51,40 +51,12 @@ $post_cost = get_field('cost');
 
 	<?php if ($post_content != ''){?>
 	<div class="event-description">
-		<!-- 
+		 
 		<div class="description-body">
 			<div class="heading-four">About this event</div>
 			<?=$post_content?></div> 
 		</div>
-		-->
-
-		<div class="description-body">
-			<div class="heading-four">About this event</div>
-			<p>What is your dream? Will you achieve it in your lifetime? I'm certain that you desire to. I'm sure you hope you will. What odds would you give yourself? One in five? One in a hundred? One in a million? How can you tell whether your chances are good or whether your dream will always remain exactly that, a dream? The more work you do to test your dream and to prepare before actually launching out to accomplish it, the greater the chance you will actually see your dream come true.</p>
-
-			<p class="end-p">Whether you have lost sight of an old dream or you are searching for a new one within you, this Put Your Dream to the Test Mastermind Group Study provides a step by step action plan that you can start using today to see, own and reach your dream. John C. Maxwell draws on his forty years of mentoring experience in this book to expertly guide you through the ten questions required of every successful dreamer.</p>
-			<div class="heading-four">Watch the video below for more info:</div>
-			<iframe style="width:100%;height:440px;" src="https://www.youtube.com/embed/q7Y5Z700DrA" frameborder="0" allowfullscreen></iframe>
-			<br>
-			
-			<p class="end-p">Certificates will be provided at the end of the 5th session.
-			If you are not satisfied with this Mastermind Group Study after attending the first session, you can get 100% of your money back, without any questions asked. Put your dreams to the test risk-free.</p>
-			
-			<div class="heading-four">Mastermind Group Study</div>
-			<p class="end-p">There is synergy of energy, commitment and excitement that participants bring to a Mastermind Group. This group is facilitated by Jade Jalali (certified John Maxwell Team member), and it offers a combination of masterminding, peer brainstorming, education, accountability and support in a group setting to sharpen your business and personal skills. By bringing fresh ideas and a different perspective, this Mastermind Group can help you achieve success.</p>
-			<div class="heading-four">Key Benefits of a Mastermind Group:</div>
-			<ul>
-				<li>Increase your own experience and confidence</li>
-				<li>Sharpen your business and personal skills</li>
-				<li>Add an instant and valuable support network</li>
-				<li>Get honest feedback, advice and brainstorming</li>
-				<li>Borrow on the experience and skills of the other members</li>
-				<li>Study in a group setting, create an action plan and have the group hold you accountable to fulfilling your plan and goals</li>
-				<li>Receive critical insights into yourself</li>
-				<li>Optimistic peer support in maintaining a positive mental attitude</li>
-				<li>A sense of shared endeavour – there are others out there</li>
-			</ul>
-		</div>
+		
 	</div>
 	<?php } ?>
 
